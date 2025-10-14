4dc789699fb854890b0451fd60f8b033c909cc13
Vyle31231022150 <Vyle.31231022150@st.ueh.edu.vn>
2025-10-14 17:46:11 +0700

    Add files via upload

---
+++ BTVN3/WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace BTVN3
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox NoiDung;
+        private System.Windows.Forms.Button btnHienThi;
+        private System.Windows.Forms.Button btnThoat;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            NoiDung = new TextBox();
+            btnHienThi = new Button();
+            btnThoat = new Button();
+            SuspendLayout();
+            // 
+            // NoiDung
+            // 
+            NoiDung.Location = new Point(89, 56);
+            NoiDung.Margin = new Padding(4, 5, 4, 5);
+            NoiDung.Multiline = true;
+            NoiDung.Name = "NoiDung";
+            NoiDung.Size = new Size(413, 62);
+            NoiDung.TabIndex = 0;
+            NoiDung.TextChanged += txtNoiDung_TextChanged;
+            // 
+            // btnHienThi
+            // 
+            btnHienThi.Location = new Point(50, 183);
+            btnHienThi.Margin = new Padding(4, 5, 4, 5);
+            btnHienThi.Name = "btnHienThi";
+            btnHienThi.Size = new Size(167, 55);
+            btnHienThi.TabIndex = 1;
+            btnHienThi.Text = "Hiển thị";
+            btnHienThi.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            btnThoat.Location = new Point(389, 174);
+            btnThoat.Margin = new Padding(4, 5, 4, 5);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(168, 55);
+            btnThoat.TabIndex = 2;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = true;
+            btnThoat.Click += btnThoat_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(609, 281);
+            Controls.Add(NoiDung);
+            Controls.Add(btnHienThi);
+            Controls.Add(btnThoat);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Form1";
+            Text = "Các sự kiện";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}
+
+
---
+++ BTVN4/BTVN4/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace BTVN4
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label So1;
+        private System.Windows.Forms.Label So2;
+        private System.Windows.Forms.Label KetQua;
+        private System.Windows.Forms.TextBox txtSo1;
+        private System.Windows.Forms.TextBox txtSo2;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.Button btnTru;
+        private System.Windows.Forms.Button btnNhan;
+        private System.Windows.Forms.Button btnChia;
+        private System.Windows.Forms.Button btnDel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            So1 = new Label();
+            So2 = new Label();
+            KetQua = new Label();
+            txtSo1 = new TextBox();
+            txtSo2 = new TextBox();
+            txtKetQua = new TextBox();
+            btnCong = new Button();
+            btnTru = new Button();
+            btnNhan = new Button();
+            btnChia = new Button();
+            btnDel = new Button();
+            SuspendLayout();
+         
+            So1.AutoSize = true;
+            So1.Location = new Point(30, 30);
+            So1.Name = "lblSo1";
+            So1.Size = new Size(105, 25);
+            So1.TabIndex = 0;
+            So1.Text = "Số thứ nhất";
+          
+            So2.AutoSize = true;
+            So2.Location = new Point(30, 70);
+            So2.Name = "So2";
+            So2.Size = new Size(93, 25);
+            So2.TabIndex = 1;
+            So2.Text = "Số thứ hai";
+           
+            KetQua.AutoSize = true;
+            KetQua.Location = new Point(30, 160);
+            KetQua.Name = "KetQua";
+            KetQua.Size = new Size(72, 25);
+            KetQua.TabIndex = 2;
+            KetQua.Text = "Kết quả";
+         
+            txtSo1.Location = new Point(148, 30);
+            txtSo1.Name = "txtSo1";
+            txtSo1.Size = new Size(180, 31);
+            txtSo1.TabIndex = 3;
+            txtSo1.TextChanged += txtSo1_TextChanged;
+        
+            txtSo2.Location = new Point(148, 67);
+            txtSo2.Name = "txtSo2";
+            txtSo2.Size = new Size(180, 31);
+            txtSo2.TabIndex = 4;
+            txtSo2.TextChanged += txtSo2_TextChanged;
+          
+            txtKetQua.Location = new Point(120, 157);
+            txtKetQua.Name = "txtKetQua";
+            txtKetQua.ReadOnly = true;
+            txtKetQua.Size = new Size(180, 31);
+            txtKetQua.TabIndex = 5;
+        
+            btnCong.Location = new Point(30, 110);
+            btnCong.Name = "btnCong";
+            btnCong.Size = new Size(40, 30);
+            btnCong.TabIndex = 6;
+            btnCong.Text = "+";
+            btnCong.UseVisualStyleBackColor = true;
+        
+            btnTru.Location = new Point(80, 110);
+            btnTru.Name = "btnTru";
+            btnTru.Size = new Size(40, 30);
+            btnTru.TabIndex = 7;
+            btnTru.Text = "-";
+            btnTru.UseVisualStyleBackColor = true;
+         
+            btnNhan.Location = new Point(130, 110);
+            btnNhan.Name = "btnNhan";
+            btnNhan.Size = new Size(40, 30);
+            btnNhan.TabIndex = 8;
+            btnNhan.Text = "*";
+            btnNhan.UseVisualStyleBackColor = true;
+          
+            btnChia.Location = new Point(180, 110);
+            btnChia.Name = "btnChia";
+            btnChia.Size = new Size(40, 30);
+            btnChia.TabIndex = 9;
+            btnChia.Text = "/";
+            btnChia.UseVisualStyleBackColor = true;
+         
+            btnDel.Location = new Point(240, 110);
+            btnDel.Name = "btnDel";
+            btnDel.Size = new Size(60, 30);
+            btnDel.TabIndex = 10;
+            btnDel.Text = "Del";
+            btnDel.UseVisualStyleBackColor = true;
+         
+            ClientSize = new Size(340, 210);
+            Controls.Add(So1);
+            Controls.Add(So2);
+            Controls.Add(KetQua);
+            Controls.Add(txtSo1);
+            Controls.Add(txtSo2);
+            Controls.Add(txtKetQua);
+            Controls.Add(btnCong);
+            Controls.Add(btnTru);
+            Controls.Add(btnNhan);
+            Controls.Add(btnChia);
+            Controls.Add(btnDel);
+            Name = "Form1";
+            Text = "Máy tính bỏ túi";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}
+
+
